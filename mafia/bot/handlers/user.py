3af2533d0074b8b12dd97465249cd845b124e8b1
pywave5 <pywave@mail.ru>
2024-11-02 05:09:08 +0500

    fix

--- mafia/bot/handlers/user.py
+++ mafia/bot/handlers/user.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import Message
-from aiogram.filters.command import CommandStart, Command
+from aiogram.filters.command import CommandStart, Command, CommandObject
 from aiogram.utils.deep_linking import create_start_link
 from aiogram.fsm.context import FSMContext
 
@@ -13,21 +13,30 @@ user = Router()
 mafia = MafiaManager()
 keyboards_control = KeyboardsControl()
 
+
 @user.message(CommandStart(deep_link=True))
-async def cmd_start(message: Message, state: FSMContext, bot) -> None:
-    link = await create_start_link(bot, "game_join", encode=True)
-    print(link) # https://t.me/rtmtrtortrbot?start=Z2FtZV9qb2lu
+async def cmd_join_game(message: Message, state: FSMContext, command: CommandObject) -> None:
+    if not mafia.player_in_game(message.from_user.id):
+        mafia.append_player(user_id=message.from_user.id,
+                            name=message.from_user.first_name)
+        await message.answer(f"Ты успешно присоединился к игре!")
+    else:
+        await message.answer(f"Ты уже в игре понимаешь? в игре :)")
+
+@user.message(CommandStart())
+async def cmd_start(message: Message, state: FSMContext) -> None:
     await message.answer(f"Привет! я бот-ведущий игры Мафия!")
     await state.clear()
 
 @user.message(CurrentChat(), Command("game"))
 async def cmd_game(message: Message, state: FSMContext, bot) -> None:
     await state.set_state(GameState.vote_to_game)
+    link = await create_start_link(bot, "game_join", encode=True)
     await message.reply(text=mafia.data["vote_to_game"],
                         reply_markup=await keyboards_control.create_inline_keyboard(
                             text=mafia.data["game_join"],
                             callback_data=None,
-                            url="https://t.me/rtmtrtortrbot?start=game_join"
+                            url=link
                         ))
 
 @user.message(CurrentChat(), Command("quit"))
