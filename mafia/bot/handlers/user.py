d1eb411d7a2196b8506990403f55778e5b282e63
pywave5 <pywave@mail.ru>
2024-11-02 15:27:46 +0500

    deep-link v1

--- mafia/bot/handlers/user.py
+++ mafia/bot/handlers/user.py
@@ -2,6 +2,7 @@ from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters.command import CommandStart, Command, CommandObject
 from aiogram.utils.deep_linking import create_start_link, decode_payload
+from aiogram.utils.text_decorations import html_decoration
 from aiogram.fsm.context import FSMContext
 
 from mafia.bot.filters.current_chat import CurrentChat
@@ -17,11 +18,13 @@ keyboards_control = KeyboardsControl()
 @user.message(CommandStart(deep_link=True))
 async def cmd_join_game(message: Message, state: FSMContext, command: CommandObject) -> None:
     args = decode_payload(payload=message.text[7:])
-    chat_name = args.split(":")[0]
+    invite_link = args.split("?")[1].split("&")[0]
+    chat_name = args.split("?")[0]
     if not mafia.player_in_game(message.from_user.id):
         mafia.append_player(user_id=message.from_user.id,
                             name=message.from_user.first_name)
-        await message.answer(f"Ты успешно присоединился к игре в <code>{chat_name}</code>")
+        await message.answer(f"Ты успешно присоединился к игре в {html_decoration.link(chat_name, invite_link)}",
+                             disable_web_page_preview=True)
     else:
         await message.answer(f"Ты уже в игре понимаешь? в игре :)")
 
@@ -34,7 +37,8 @@ async def cmd_start(message: Message, state: FSMContext) -> None:
 async def cmd_game(message: Message, state: FSMContext, bot) -> None:
     await state.set_state(GameState.vote_to_game)
     chat_name = message.chat.title
-    link = await create_start_link(bot, f"{chat_name}:game_join", encode=True)
+    invite_link = await message.bot.create_chat_invite_link(chat_id=message.chat.id)
+    link = await create_start_link(bot, f"{chat_name}?{invite_link.invite_link}", encode=True)
     await message.reply(text=mafia.data["vote_to_game"],
                         reply_markup=await keyboards_control.create_inline_keyboard(
                             text=mafia.data["game_join"],
