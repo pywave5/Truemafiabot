71c4946c834988e889fe48d096ca38a9ddc453ff
pywave5 <pywave@mail.ru>
2024-11-02 12:59:44 +0500

    deep-link

--- mafia/bot/handlers/user.py
+++ mafia/bot/handlers/user.py
@@ -1,7 +1,7 @@
 from aiogram import Router, F
 from aiogram.types import Message
 from aiogram.filters.command import CommandStart, Command, CommandObject
-from aiogram.utils.deep_linking import create_start_link
+from aiogram.utils.deep_linking import create_start_link, decode_payload
 from aiogram.fsm.context import FSMContext
 
 from mafia.bot.filters.current_chat import CurrentChat
@@ -16,10 +16,12 @@ keyboards_control = KeyboardsControl()
 
 @user.message(CommandStart(deep_link=True))
 async def cmd_join_game(message: Message, state: FSMContext, command: CommandObject) -> None:
+    args = decode_payload(payload=message.text[7:])
+    chat_name = args.split(":")[0]
     if not mafia.player_in_game(message.from_user.id):
         mafia.append_player(user_id=message.from_user.id,
                             name=message.from_user.first_name)
-        await message.answer(f"Ты успешно присоединился к игре!")
+        await message.answer(f"Ты успешно присоединился к игре в <code>{chat_name}</code>")
     else:
         await message.answer(f"Ты уже в игре понимаешь? в игре :)")
 
@@ -31,7 +33,8 @@ async def cmd_start(message: Message, state: FSMContext) -> None:
 @user.message(CurrentChat(), Command("game"))
 async def cmd_game(message: Message, state: FSMContext, bot) -> None:
     await state.set_state(GameState.vote_to_game)
-    link = await create_start_link(bot, "game_join", encode=True)
+    chat_name = message.chat.title
+    link = await create_start_link(bot, f"{chat_name}:game_join", encode=True)
     await message.reply(text=mafia.data["vote_to_game"],
                         reply_markup=await keyboards_control.create_inline_keyboard(
                             text=mafia.data["game_join"],
